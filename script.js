05bb6f374b39505f0af27f32795bfb71e199735b
Kasia <kasia.bondarava@gmail.com>
2021-07-21 16:39:33 -0400

    Styling and functionality for buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleBtns = document.querySelectorAll(".faq-toggle");
+
+
+toggleBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.parentNode.classList.toggle("active");
+    })
+})
